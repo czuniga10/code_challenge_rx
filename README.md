47a43f9110b763b420b8f6c82eca31fb0f29947f
Chad <chad.zuniga10@gmail.com>
2019-04-14 01:15:02 -0600

    Material UI form for all inputs

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 ### Node.js => PostgreSQL Backend 
 #### Backend Installation Instructions
-+ In your terminal, cd into my project
++ In your terminal, cd into code_challenge_rx
 + run ``yarn install`` (installs necessary dependencies) <br>
 + run ``nodemon`` OR ``node server/index.js``
 
+_______________________________________________________________
 <br>
 
 ### Axios => React | Redux Frontend
 #### Frontend Installation Instructions
++ In your terminal, cd into cc_frontend
++ run ``yarn install`` (installs necessary dependencies) <br>
++ run ``yarn start``
--- cc_frontend/src/App.js
+++ cc_frontend/src/App.js
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  }
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props) 
+      this.state={
+
+      }
+  }
+
+  componentDidMount(){
+    
+  }
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
-        hello world
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-dense"
+            label="First Name"
+            className={classNames(classes.textField, classes.dense)}
+            margin="dense"
+          />
+          <TextField
+            id="standard-dense"
+            label="Last Name"
+            className={classNames(classes.textField, classes.dense)}
+            margin="dense"
+          />
+          <TextField
+            id="standard-dense"
+            label="Phone Number"
+            className={classNames(classes.textField, classes.dense)}
+            margin="dense"
+          />
+          <TextField
+            id="standard-dense"
+            label="Email"
+            className={classNames(classes.textField, classes.dense)}
+            margin="dense"
+          />
+        </form>
+
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
